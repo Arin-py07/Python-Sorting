82debe8204162f4c8dac41494a8066d00fac14ae
Arindam Dey <deyarindam475@gmail.com>
2021-06-29 22:08:45 +0530

    Create Bubblesort.py

---
+++ Bubblesort.py
@@ -0,0 +1,46 @@
+#native way:
+
+def bubbleSort(l):
+    n = len(l)
+
+    for i in range(n-1):
+
+        for j in range(n - i-1):
+            if l[j] > l[j + 1]:
+                l[j], l[j + 1] = l[j + 1], l[j]
+
+
+l = [10, 8, 20, 5]
+
+bubbleSort(l)
+
+print(*l)
+
+output:
+5 8 10 20
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#effective way
+
+def bubbleSort(l):
+    n = len(l)
+
+    for i in range(n-1):
+        swapped = False
+
+        for j in range(n-i-1):
+            if l[j]>l[j+1]:
+                l[j],l[j+1] = l[j+1],l[j]
+
+                swapped = True
+
+        if swapped == False:
+            return
+
+l = [10, 8, 20, 5]
+
+bubbleSort(l)
+
+print(*l)
+
+
+output: 5 8 10 20
